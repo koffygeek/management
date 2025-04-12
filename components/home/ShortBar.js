cf6b9208c0448018a203bf18dd8d19daedc20a07
Yvan Grebe <yvan.grebe@gmail.com>
2025-04-12 14:06:17 +0100

    state isFreelanceHandler

--- components/home/ShortBar.js
+++ components/home/ShortBar.js
@@ -16,14 +16,18 @@ const ShortBar = () => {
       <div className="py-10 w-[95%] lg:w-[90%] xl:w-[80%] mx-auto flex justify-center items-end md:justify-end  ">
         <ul className="flex space-x-5 items-center">
           <li className="flex items-center space-x-2">
-            <span className="text-[1rem] md:text-[1.2rem] font-bold ">Freelance</span>
-            <div>
-              <div></div>
+            <span className="text-[1rem] md:text-[1.2rem] font-bold ">
+              Freelance
+            </span>
+            <div className="relative inline-block w-12 h-6">
+              <div onClick={isFreelanceHandler} className="relative cursor-pointer inline-block w-12 h-6 rounded-full bg-[#91d327]  "> <span className="absolute w-6 h-6 bg-white rounded-full shadow-md "></span>  </div>
             </div>
           </li>
           <li className="flex items-center space-x-2">
-            <span className="text-[1rem] md:text-[1.2rem] font-bold " >Plein Emploi</span>
-            <div>
+            <span className="text-[1rem] md:text-[1.2rem] font-bold ">
+              Plein Emploi
+            </span>
+            <div className="relative inline-block w-12 h-6">
               <div></div>
             </div>
           </li>
